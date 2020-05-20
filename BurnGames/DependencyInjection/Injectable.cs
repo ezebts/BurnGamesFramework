ba9e2e103aada3f0cc33d6544b8a1d7e908e4c4a
Ezequiel Bitschin <ezequiel.bitschin@gmail.com>
2020-05-19 21:40:35 -0300

    Initialize repository

---
+++ BurnGames/DependencyInjection/Injectable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BurnGames.DependencyInjection
+{
+
+    public class Injectable : PropertyAttribute
+    {
+
+        public string DisplayName { get; set; } = null;
+
+        public Type InjectedType { get; private set; }
+        
+        public bool IsValid(object reference)
+        {
+            return InjectedType.IsInstanceOfType(reference);
+        }
+
+        public Injectable(Type injectable)
+        {
+            InjectedType = injectable;
+        }
+
+    }
+
+}
