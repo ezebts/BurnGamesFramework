5530cf4799cc54c3270b19064742d64ee306a5fc
ezebts <ezequiel.bitschin@gmail.com>
2020-05-29 18:13:13 -0300

    Add new event system implementation

---
+++ BurnGames/Events/GameEvent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BurnGames.Events
+{
+
+    /// <summary>
+    /// 
+    /// Game event system based on scriptable objects
+    /// 
+    /// </summary>
+    [CreateAssetMenu(menuName = "BurnGames/Events/Game Event", fileName = "New Game Event")]
+    public class GameEvent : ScriptableObject
+    {
+
+        private List<GameEventListener> listeners = new List<GameEventListener>();
+
+        [SerializeField] bool enabled = true;
+
+        public bool Enabled { get => enabled; set => enabled = value; }
+
+        public void AddListener(GameEventListener eventListener)
+        {
+            listeners.Add(eventListener);
+        }
+
+        public void RemoveListener(GameEventListener eventListener)
+        {
+            listeners.Remove(eventListener);
+        }
+
+        public void Invoke()
+        {
+            if (Enabled)
+            {
+
+                foreach(var listener in listeners)
+                {
+                    listener.EventActions.Invoke();
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ BurnGames/Events/GameEventListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BurnGames.Events
+{
+
+    [AddComponentMenu("BurnGames/Events/Game Event Listener", 1)]
+    public class GameEventListener : MonoBehaviour
+    {
+
+        [SerializeField] GameEvent _event = null;
+
+        [SerializeField] private UnityEvent eventActions = new UnityEvent();
+
+        public UnityEvent EventActions => eventActions;
+
+        void OnEnable()
+        {
+            _event.AddListener(this);
+        }
+
+        void OnDisable()
+        {
+            _event.RemoveListener(this);
+        }
+
+    }
+
+}
